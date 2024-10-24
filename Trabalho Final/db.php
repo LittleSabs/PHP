2e51984daed54b7b4d5f8cef03e363f48b83d7cf
Sabrina Vianna <sabrina.vianna@sempreceub.com>
2024-10-24 11:56:24 -0300

    Update db.php

--- Trabalho Final/db.php
+++ Trabalho Final/db.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "localhost"; // ou seu servidor
-$username = "root"; // seu usuário do MySQL
-$password = "Sabrina@05"; // sua senha do MySQL
+$servername = "localhost"; 
+$username = "root"; 
+$password = "Sabrina@05"; 
 $dbname = "php_db";
 
-// Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexão
+
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
